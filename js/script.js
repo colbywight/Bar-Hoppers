6e28c8c845ec47f3c1772c6fb720277c32b2fa96
mkumij37 <ucheckmyemail@hotmail.com>
2019-11-19 11:32:38 -0700

    trying to get appending of data to work added css elements

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let featMatrix = new FeatureMatrix();
 // let shiftChart = new SelectionChart();
 //
 // let electoralVoteChart = new FundingMap(shiftChart);
-// let mapChart = new MapChart();
+let mapChart = new MapChart();
 
 
 // Load the data corresponding to all the election years.
@@ -23,7 +23,8 @@ let featMatrix = new FeatureMatrix();
 d3.csv("data/2015StateScoresAndExpenses.csv").then(stateScores => {
   console.log(stateScores);
   let mapChart = new MapChart(stateScores);
-  mapChart.update()
+  mapChart.update();
+  featMatrix.update();
   // let yearChart = new MainMenu(electoralVoteChart, tileChart,
   //     votePercentageChart, electionWinners);
   // yearChart.update();
